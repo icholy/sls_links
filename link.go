379b250b25ceb01a29eacc2f59226f948544ee77
Ilia Choly <ilia.choly@gmail.com>
2021-08-12 18:42:21 -0400

    use QueryEscape function

--- link.go
+++ link.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/url"
 	"strings"
 )
 
@@ -12,8 +11,8 @@ func Link(region, name string) string {
 
 func LogLink(region, lambda string) string {
 	group := "/aws/lambda/" + lambda
-	group = url.QueryEscape(group)
-	group = url.QueryEscape(group)
+	group = QueryEscape(group)
+	group = QueryEscape(group)
 	group = strings.ReplaceAll(group, "%", "$")
 	return fmt.Sprintf(
 		"https://console.aws.amazon.com/cloudwatch/home?region=%s#logsV2:log-groups/log-group/%s",
